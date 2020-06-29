14590155a6671b2501c8910c5508a698df2fd90f
spGroup7project2020 <spGroup7project2020@gmail.com>
2020-06-29 02:43:32 +0200

    Login + Sign Up design

---
+++ app/src/main/java/com/dube/ashley/pearsonhub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dube.ashley.pearsonhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
