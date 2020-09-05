93ad7526a85cf4acb51515c4f5b25f797fc094f6
andy777-droid <hart.andrew117@gmail.com>
2020-09-05 03:47:24 +0200

    navigation bar

--- app/src/main/java/com/dube/ashley/pearsonhub/HomeActivity.java
+++ app/src/main/java/com/dube/ashley/pearsonhub/HomeActivity.java
@@ -1,79 +1,119 @@
 package com.dube.ashley.pearsonhub;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
-import android.content.Intent;
+
+import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity
-{
-    Button signOutBTN;
-    FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private ViewPager viewPager;
-    private SliderAdapter sliderAdapter;
-    private LinearLayout mDotsSlider;
-    private TextView[] mdots;
+public class HomeActivity extends AppCompatActivity {
+  Button signOutBTN;
+  FirebaseAuth mFirebaseAuth;
+  private FirebaseAuth.AuthStateListener mAuthStateListener;
+  private ViewPager viewPager;
+  private SliderAdapter sliderAdapter;
+  private LinearLayout mDotsSlider;
+  private TextView[] mdots;
+  private DrawerLayout mdrawer;
+  private ActionBarDrawerToggle mToggle;
+
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+
+    mdrawer = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+    mToggle = new ActionBarDrawerToggle(this, mdrawer, R.string.open, R.string.close);
+
+    mdrawer.addDrawerListener(mToggle);
+    mToggle.syncState();
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    setTitle("");
+    getSupportActionBar().setElevation(0);
+    viewPager = (ViewPager) findViewById(R.id.SlideViewPager);
+    mDotsSlider = (LinearLayout) findViewById(R.id.dots);
+
+    sliderAdapter = new SliderAdapter(this);
+    viewPager.setAdapter(sliderAdapter);
+    addDotsSlider(0);
+    viewPager.addOnPageChangeListener(viewListener);
+
+    mToggle.setDrawerArrowDrawable(new HamburgerDrawable(this));
+
+  }
+
+  public class HamburgerDrawable extends DrawerArrowDrawable {
+
+    public HamburgerDrawable(Context context){
+      super(context);
+      setColor(context.getResources().getColor(R.color.colorPrimary));
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    public void draw(Canvas canvas){
+      super.draw(canvas);
 
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_home);
-
-            viewPager = (ViewPager) findViewById(R.id.SlideViewPager);
-            mDotsSlider = (LinearLayout) findViewById(R.id.dots);
-
-            sliderAdapter = new SliderAdapter(this);
-            viewPager.setAdapter(sliderAdapter);
-            addDotsSlider(0);
-            viewPager.addOnPageChangeListener(viewListener);
+      setBarLength(100.0f);
+      setBarThickness(16.0f);
+      setGapSize(20.0f);
 
     }
+  }
 
-    public void addDotsSlider(int indicator){
-        mdots = new TextView[6];
-        mDotsSlider.removeAllViews();
-
-        for (int i = 0; i < mdots.length; i++){
-            mdots[i] = new TextView(this);
-            mdots[i].setText(Html.fromHtml("&#8226"));
-            mdots[i].setTextSize(35);
-            mdots[i].setTextColor(getResources().getColor(R.color.unchecked));
-
-            mDotsSlider.addView(mdots[i]);
-
-
-        }
-        if (mdots.length > 0){
-            mdots[indicator].setTextColor(getResources().getColor(R.color.checked));
-        }
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    if (mToggle.onOptionsItemSelected(item)) {
+      return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
 
-    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener()  {
+  public void addDotsSlider(int indicator) {
+    mdots = new TextView[6];
+    mDotsSlider.removeAllViews();
+
+    for (int i = 0; i < mdots.length; i++) {
+      mdots[i] = new TextView(this);
+      mdots[i].setText(Html.fromHtml("&#8226"));
+      mdots[i].setTextSize(35);
+      mdots[i].setTextColor(getResources().getColor(R.color.unchecked));
+
+      mDotsSlider.addView(mdots[i]);
+    }
+    if (mdots.length > 0) {
+      mdots[indicator].setTextColor(getResources().getColor(R.color.checked));
+    }
+  }
+
+  ViewPager.OnPageChangeListener viewListener =
+      new ViewPager.OnPageChangeListener() {
 
         @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
 
         @Override
         public void onPageSelected(int position) {
-           addDotsSlider(position);
+          addDotsSlider(position);
         }
 
         @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-    };
-
+        public void onPageScrollStateChanged(int state) {}
+      };
 }
--- app/src/main/java/com/dube/ashley/pearsonhub/LoginActivity.java
+++ app/src/main/java/com/dube/ashley/pearsonhub/LoginActivity.java
@@ -18,135 +18,115 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity
-{
-    EditText emailId, passwordTXT;
-    Button btnSignIn;
-    TextView SignUp, resetPsw;
-    FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
+public class LoginActivity extends AppCompatActivity {
+  EditText emailId, passwordTXT;
+  Button btnSignIn;
+  TextView SignUp, resetPsw;
+  FirebaseAuth mFirebaseAuth;
+  private FirebaseAuth.AuthStateListener mAuthStateListener;
+  public String valid_user = "lol";
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    mFirebaseAuth = FirebaseAuth.getInstance();
+    emailId = findViewById(R.id.editTextTextEmailAddress);
+    passwordTXT = findViewById(R.id.editTextTextPassword);
 
-       }
+    SignUp = findViewById(R.id.signUpTV);
+    SpannableString content1 = new SpannableString("Create Account");
+    content1.setSpan(new UnderlineSpan(), 0, content1.length(), 0);
+    SignUp.setText(content1);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        emailId=findViewById(R.id.editTextTextEmailAddress);
-        passwordTXT=findViewById(R.id.editTextTextPassword);
+    resetPsw = findViewById(R.id.linkForgot);
+    SpannableString content2 = new SpannableString("Forgot Password?");
+    content2.setSpan(new UnderlineSpan(), 0, content2.length(), 0);
+    resetPsw.setText(content2);
 
-        SignUp=findViewById(R.id.signUpTV);
-        SpannableString content1 = new SpannableString("Create Account");
-        content1.setSpan(new UnderlineSpan(), 0, content1.length(), 0);
-        SignUp.setText(content1);
+    btnSignIn = findViewById(R.id.signInBTN);
 
-        resetPsw=findViewById(R.id.linkForgot);
-        SpannableString content2 = new SpannableString("Forgot Password?");
-        content2.setSpan(new UnderlineSpan(), 0, content2.length(), 0);
-        resetPsw.setText(content2);
-
-        btnSignIn=findViewById(R.id.signInBTN);
-
-        mAuthStateListener= new FirebaseAuth.AuthStateListener()
-        {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
-            {
-                FirebaseUser mFirebaseUser =mFirebaseAuth.getCurrentUser();
-                if(mFirebaseUser != null && mFirebaseUser.isEmailVerified())
-                {
-                    Toast.makeText(LoginActivity.this,"You're logged in",Toast.LENGTH_SHORT).show();
-                    Intent i=new Intent(LoginActivity.this,HomeActivity.class);
-                    startActivity(i);
-                }
-
+    mAuthStateListener =
+        new FirebaseAuth.AuthStateListener() {
+          @Override
+          public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+            if (mFirebaseUser != null && mFirebaseUser.isEmailVerified()) {
+              Toast.makeText(LoginActivity.this, "You're logged in", Toast.LENGTH_SHORT).show();
+              Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+              startActivity(i);
             }
+          }
         };
 
-        btnSignIn.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                String email=emailId.getText().toString();
-                String passwrd=passwordTXT.getText().toString();
+    btnSignIn.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            String email = emailId.getText().toString();
+            String passwrd = passwordTXT.getText().toString();
 
-                if(email.isEmpty())
-                {
-                    emailId.setError("Please enter your email address");
-                    emailId.requestFocus();
-                }
-                else if (passwrd.isEmpty())
-                {
-                    passwordTXT.setError("Please enter your password");
-                    passwordTXT.requestFocus();
-                }
-                else if(email.isEmpty() && passwrd.isEmpty())
-                {
-                    Toast.makeText(LoginActivity.this,"Fields are empty",Toast.LENGTH_SHORT).show();
-                }
-                else if(!(email.isEmpty() && passwrd.isEmpty()))
-                {
-                    mFirebaseAuth.signInWithEmailAndPassword(email,passwrd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>()
-                    {
+            if (email.isEmpty()) {
+              emailId.setError("Please enter your email address");
+              emailId.requestFocus();
+            } else if (passwrd.isEmpty()) {
+              passwordTXT.setError("Please enter your password");
+              passwordTXT.requestFocus();
+            } else if (email.isEmpty() && passwrd.isEmpty()) {
+              Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+            } else if (!(email.isEmpty() && passwrd.isEmpty())) {
+              mFirebaseAuth
+                  .signInWithEmailAndPassword(email, passwrd)
+                  .addOnCompleteListener(
+                      LoginActivity.this,
+                      new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task)
-                        {
-                            if(!task.isSuccessful())
-                            {
-                                Toast.makeText(LoginActivity.this,"Login Error, please attempt Login again",Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                if(mFirebaseAuth.getCurrentUser().isEmailVerified())
-                                {
-                                    Intent intentHome=new Intent(LoginActivity.this,HomeActivity.class);
-                                    startActivity(intentHome);
-                                }
-                                else
-                                {
-                                    Toast.makeText(LoginActivity.this,"Please verify your email address",Toast.LENGTH_LONG).show();
-                                }
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                          if (!task.isSuccessful()) {
+                            Toast.makeText(
+                                    LoginActivity.this,
+                                    "Login Error, please attempt Login again",
+                                    Toast.LENGTH_SHORT)
+                                .show();
+                          } else {
+                            if (mFirebaseAuth.getCurrentUser().isEmailVerified()) {
 
+                                   Intent intentHome =
+                                  new Intent(LoginActivity.this, HomeActivity.class);
+                              startActivity(intentHome);
+                            } else {
+                              Toast.makeText(
+                                      LoginActivity.this,
+                                      "Please verify your email address",
+                                      Toast.LENGTH_LONG)
+                                  .show();
                             }
+                          }
                         }
-                    });
-                }
-                else
-                {
-                    Toast.makeText(LoginActivity.this,"Error Occured",Toast.LENGTH_SHORT).show();
-                }
-
+                      });
+            } else {
+              Toast.makeText(LoginActivity.this, "Error Occured", Toast.LENGTH_SHORT).show();
             }
+          }
         });
 
-        SignUp.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intentSignUp=new Intent(LoginActivity.this,SignUpActivity.class);
-                startActivity(intentSignUp);
-            }
+    SignUp.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent intentSignUp = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intentSignUp);
+          }
         });
 
-        resetPsw.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intentReset=new Intent(LoginActivity.this,ResetPassword.class);
-                startActivity(intentReset);
-            }
+    resetPsw.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent intentReset = new Intent(LoginActivity.this, ResetPassword.class);
+            startActivity(intentReset);
+          }
         });
-                //Hello
-    }
+    // Hello
+  }
 }
