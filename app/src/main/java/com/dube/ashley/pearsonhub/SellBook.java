33d6f16451b31753971f06294c22a00a09e9da86
andy777-droid <hart.andrew117@gmail.com>
2020-09-10 10:13:34 +0200

    added sell page

---
+++ app/src/main/java/com/dube/ashley/pearsonhub/SellBook.java
@@ -0,0 +1,14 @@
+package com.dube.ashley.pearsonhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_book);
+    }
+}
