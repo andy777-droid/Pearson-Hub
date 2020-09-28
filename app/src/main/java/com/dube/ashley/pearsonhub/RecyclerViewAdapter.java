d00fc8435d045bbcc905a05ad4995185c817089d
Ashley Dube <ashleydee1999@gmail.com>
2020-09-28 16:39:50 +0200

    Set a Click listener on the SearchBook CardView activity.

--- app/src/main/java/com/dube/ashley/pearsonhub/RecyclerViewAdapter.java
+++ app/src/main/java/com/dube/ashley/pearsonhub/RecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.dube.ashley.pearsonhub;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
@@ -31,11 +33,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
         return new MyViewHolder(view);
     }
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position)
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position)
     {
         holder.tv_book_title.setText(mData.get(position).getTitle());
         holder.tv_book_price.setText("R"+mData.get(position).getPrice());
-        holder.tv_book_price.setText("R "+mData.get(position).getPrice());
 
         Picasso.Builder builder = new Picasso.Builder(mContext);
         builder.listener(new Picasso.Listener()
@@ -47,6 +48,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
             }
         });
         builder.build().load(mData.get(position).getThumbnail()).into(holder.img_book_thumbnail);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent(mContext,Book.class);
+                intent.putExtra("Thumbnail",mData.get(position).getThumbnail());
+                intent.putExtra("Title",mData.get(position).getTitle());
+                intent.putExtra("Price",mData.get(position).getPrice());
+                intent.putExtra("SellerNumber",mData.get(position).getSellerNumber());
+                intent.putExtra("SellerName",mData.get(position).getSellerName());
+                intent.putExtra("Author",mData.get(position).getAuthor());
+                intent.putExtra("Condition",mData.get(position).getCondition());
+                intent.putExtra("ISBN",mData.get(position).getISBN());
+                mContext.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
@@ -59,12 +78,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
         TextView tv_book_title;
         TextView tv_book_price;
         ImageView img_book_thumbnail;
+        CardView cardView;
         public MyViewHolder(@NonNull View itemView)
         {
             super(itemView);
             tv_book_title=(TextView) itemView.findViewById(R.id.book_title_id);
             tv_book_price=(TextView) itemView.findViewById(R.id.book_Price_id);
             img_book_thumbnail=(ImageView) itemView.findViewById(R.id.book_img_id);
+            cardView=(CardView) itemView.findViewById(R.id.cardViewBook_id);
         }
     }
 }
